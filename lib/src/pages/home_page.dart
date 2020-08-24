8b8d20cf119ffd9f856106194085d094f03bdb76
Daniel Maximiliano Gómez Cantero <danielgomezcantero@gmail.com>
2020-08-24 08:55:15 -0400

    Configuración de rutas

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+    );
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Mapas')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_5), title: Text('Direcciones')),
+      ],
+    );
+  }
+
+  Widget callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
+  }
+}
