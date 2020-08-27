b76bba98d0f69f5d25a4b5a07e1ce69875e85deb
Daniel Maximiliano Gómez Cantero <danielgomezcantero@gmail.com>
2020-08-27 14:39:17 -0400

    Create and Insert SQFLite

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DBProvider {
   static Database _database;
@@ -13,5 +17,44 @@ class DBProvider {
     return _database;
   }
 
-  initDB() async {}
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database db, int version) async {
+        await db.execute('CREATE TABLE Scans( '
+            'id INTEGER PRIMARY KEY,'
+            'tipo TEXT,'
+            'valor TEXT'
+            ' )');
+      },
+    );
+  }
+
+  //Crear Registros
+
+  nuevoScanRaw(ScanModel nuevoScan) async {
+    // se verifica la disponibilidad de la BD
+    final db = await database;
+
+    final res = await db.rawInsert("INSERT INTO Scans (id, tipo, valor) "
+        "VALUES (${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor}' )");
+
+    return res;
+  }
+
+  nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = await db.insert('Scans', nuevoScan.toJson());
+    return res;
+  }
+
+  //Select - Obtener Informacion
+
 }
